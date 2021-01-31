5e79ec4acc045c0c15c8d037c8a22956a12f0386
Cyroxin <34924561+Cyroxin@users.noreply.github.com>
2021-01-31 19:36:13 +0200

    Create carousel

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView, useWindowDimensions } from 'react-native';
+import Carousel from './Components/Carousel';
 
 export default function App() {
+  const width = useWindowDimensions().width;
+  const height = useWindowDimensions().height;
+
   return (
-    <View style={styles.container}>
-      <Image source = {{uri:'https://pbs.twimg.com/profile_images/486929358120964097/gNLINY67_400x400.png'}} style={{width: 200, height: 200}}></Image>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Carousel>
+      <Image source = {{uri:`https://dummyimage.com/800x400/daabdb/000000`}} style={{width: width, height: 300, resizeMode: 'cover', aspectRatio: 800/400}}></Image>
+      <Image source = {{uri:`https://dummyimage.com/800x400/dbbbdb/000000`}} style={{width: width, height: 300, resizeMode: 'cover', aspectRatio: 800/400}}></Image>
+      <Image source = {{uri:`https://dummyimage.com/800x400/dccbdb/000000`}} style={{width: width, height: 300, resizeMode: 'cover', aspectRatio: 800/400}}></Image>
+      <Image source = {{uri:`https://dummyimage.com/800x400/dddbdb/000000`}} style={{width: width, height: 300, resizeMode: 'cover', aspectRatio: 800/400}}></Image>
+      <Image source = {{uri:`https://dummyimage.com/800x400/deebdb/000000`}} style={{width: width, height: 300, resizeMode: 'cover', aspectRatio: 800/400}}></Image>
+      </Carousel>
+      <Text style={{margin: 10, alignSelf: 'center'}}>Open up App.js to start working on your app!</Text>
     </View>
   );
 }
@@ -18,5 +27,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+    width: '100%',
+  }
 });
---
+++ Components/Carousel.js
@@ -0,0 +1,93 @@
+import React, { useEffect } from 'react';
+import { View, ScrollView, Text, Platform } from 'react-native';
+
+export const Carousel = (props) => {
+  const items = props.children;
+
+  const itemsPerInterval =
+    props.itemsPerInterval === undefined ? 1 : props.itemsPerInterval;
+
+  const [scroll, setScroll] = React.useState();
+  const [interval, setInterval] = React.useState(1);
+  const [intervals, setIntervals] = React.useState(1);
+  const [width, setWidth] = React.useState(0);
+
+  const init = (width) => {
+    // initialise width
+    setWidth(width);
+    // initialise total intervals
+    const totalItems = props.children.length;
+    setIntervals(Math.ceil(totalItems / itemsPerInterval));
+  };
+
+  const getInterval = (offset) => {
+    /*
+    for (let i = 1; i <= intervals; i++) {
+      if (offset + 1 < (width / intervals) * i) {
+        return i;
+      }
+      if (i == intervals) {
+        return i;
+      }
+    }*/
+    return offset / (width / intervals) + 1;
+  };
+
+  const getOffset = (interval) => {
+    return (width / intervals) * (interval - 1);
+  };
+
+  let bullets = [];
+  for (let i = 1; i <= intervals; i++) {
+    bullets.push(
+      <Text
+        key={i}
+        onPress= {(e) => {
+          scroll.scrollTo({ x: getOffset(i), animated: true });
+        }}
+        style={{
+          fontSize: 50,
+          paddingHorizontal: 10,
+          opacity: interval === i ? 0.5 : 0.1,
+        }}
+      >
+        &bull;
+      </Text>
+    );
+  }
+
+  return (
+    <View style={{display: 'flex', flexDirection: 'column'}}>
+      <ScrollView
+        horizontal={true}
+        ref={(scroller) => {
+          setScroll(scroller);
+        }}
+        contentContainerStyle={{
+          width: `${100 * intervals}%`,
+          justifyContent: 'center'
+        }}
+        showsHorizontalScrollIndicator={false}
+        onContentSizeChange={(w, h) => init(w)}
+        onScroll={(data) => {
+          // Set the width to match the slide, not needed if slide sizes do not change/differ.
+          setWidth(data.nativeEvent.contentSize.width);
+          // Remember the current slide
+          setInterval(getInterval(data.nativeEvent.contentOffset.x));
+        }}
+        scrollEventThrottle={0} // Do not show bullet opacity change done by animation
+        pagingEnabled
+        decelerationRate='normal'
+        disableIntervalMomentum={true} // Max slide change is (+/-)1
+        snapToInterval={width / intervals}
+      >
+        {items}
+      </ScrollView>
+      <View style={{display: 'flex', flexDirection: 'row', justifyContent: 'center'} }>
+        {bullets}
+      </View>
+    </View>
+  );
+};
+
+export default Carousel;
